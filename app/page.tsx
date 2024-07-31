ef0ca16084aee56a3db57526f2c7d6c98eb4a0a4
Joy <jungheeyoo.joy@gmail.com>
2024-07-31 18:12:57 +0900

    State Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -80,32 +80,61 @@
 //   );
 // }
 
+// // ---------------------------------------------------------
+// // 3-6
+// // Form Modifiers
+// // 그라데이션
+// // invalid
+// // peer
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
+//       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+//         <input
+//           className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+//           type="email"
+//           required
+//           placeholder="Email address"
+//         />
+//         <span className="text-red-500 font-medium hidden peer-invalid:block">
+//           Email is required.
+//         </span>
+//         <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-required:bg-green-500">
+//           Log in
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
 // ---------------------------------------------------------
-// 3-6
-// Form Modifiers
-// 그라데이션
-// invalid
-// peer
+// 3-7
+// State Modifiers
+// 자식 선택자
+// *:
+// has
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
+    <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+      <div
+        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
+      has-[:invalid]:ring-red-100"
+      >
         <input
-          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
+          className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="text"
           required
           placeholder="Email address"
         />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
+        <span className="text-red-500 font-medium hidden peer-invalid:block ">
           Email is required.
         </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-required:bg-green-500">
+        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
           Log in
         </button>
       </div>
     </main>
   );
 }
-
-//
