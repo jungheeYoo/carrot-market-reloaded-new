57a779294b7dc2e563d642d9e6e7970598a14d43
Joy <jungheeyoo.joy@gmail.com>
2024-07-31 19:08:38 +0900

    Lists and Animations animate-pulse

--- app/page.tsx
+++ app/page.tsx
@@ -144,19 +144,18 @@
 // Lists and Animations
 // odd: , even: 홀짝 스타일 주기
 // first, last
-// animation bounce, spin
+// animate-bounce, spin
+// animate-pulse : skeleton : loaing state
 
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 ">
+          <div key={index} className="flex items-center gap-5 *:animate-pulse ">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 animate-spin bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>⏳</span>
-            </div>
+            <div className="w-40 h-4 rounded-full bg-gray-400" />
+            <div className="w-20 h-4 rounded-full bg-gray-400" />
           </div>
         ))}
       </div>
