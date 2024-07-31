215e996c4aa6f4ea134dc882df26b600733e8704
Joy <jungheeyoo.joy@gmail.com>
2024-07-31 20:16:47 +0900

    Group Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -139,24 +139,65 @@
 //   );
 // }
 
+// // ---------------------------------------------------------
+// // 3-8
+// // Lists and Animations
+// // odd: , even: 홀짝 스타일 주기
+// // first, last
+// // animate-bounce, spin
+// // animate-pulse : skeleton : loaing state
+// // animate-ping : notifications 같은 것에 사용
+// // empty state
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
+//         {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
+//           <div key={index} className="flex items-center gap-5 ">
+//             <div className="size-10 bg-blue-400 rounded-full " />
+//             <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+//               {person}
+//             </span>
+//             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+//               <span className="z-10">{index}</span>
+//               <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </main>
+//   );
+// }
+
 // ---------------------------------------------------------
-// 3-8
-// Lists and Animations
-// odd: , even: 홀짝 스타일 주기
-// first, last
-// animate-bounce, spin
-// animate-pulse : skeleton : loaing state
-// animate-ping : notifications 같은 것에 사용
-// empty state
+// 3-9
+// Group Modifiers
+// 부모 state에 따라 자식 스타일을 수정
+// group-hover:
+// group-focus-within:
+// 그룹이 내부에 포커스 된 input을 갖고 있을 때만 true가 됨
 
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
-        {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 ">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {/* group-focus-within: */}
+        <div className="group flex flex-col">
+          <input
+            className="bg-gray-100 w-full"
+            placeholder="Write your email"
+          />
+          <span className="group-focus-within:block hidden">
+            Make sure it tis a valid email...
+          </span>
+          <button>Submit</button>
+        </div>
+        {/* group-hover: */}
+        {/* {['Nico', 'Me', 'You', 'Yourself', ''].map((person, index) => (
+          <div key={index} className="flex items-center group gap-5 ">
             <div className="size-10 bg-blue-400 rounded-full " />
-            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
               {person}
             </span>
             <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
@@ -164,7 +205,7 @@ export default function Home() {
               <div className="size-6 bg-red-500 rounded-full absolute animate-ping"></div>
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </main>
   );
