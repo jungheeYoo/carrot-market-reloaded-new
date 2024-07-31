c9ded62fee33acb34ea3754278991504321b67a6
Joy <jungheeyoo.joy@gmail.com>
2024-07-31 18:39:29 +0900

    Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -108,32 +108,58 @@
 //   );
 // }
 
+// // ---------------------------------------------------------
+// // 3-7
+// // State Modifiers
+// // 자식 선택자
+// // *:
+// // has
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
+//       <div
+//         className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
+//       has-[:invalid]:ring-red-100"
+//       >
+//         <input
+//           className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+//           type="text"
+//           required
+//           placeholder="Email address"
+//         />
+//         <span className="text-red-500 font-medium hidden peer-invalid:block ">
+//           Email is required.
+//         </span>
+//         <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
+//           Log in
+//         </button>
+//       </div>
+//     </main>
+//   );
+// }
+
 // ---------------------------------------------------------
-// 3-7
-// State Modifiers
-// 자식 선택자
-// *:
-// has
+// 3-8
+// Lists and Animations
+// odd: , even: 홀짝 스타일 주기
 
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div
-        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow
-      has-[:invalid]:ring-red-100"
-      >
-        <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block ">
-          Email is required.
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90  transition-transform font-medium  md:px-10 bg-black ">
-          Log in
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl"
+          >
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
