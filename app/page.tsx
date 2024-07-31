05414bb92d2d3aa2796d77304a545264fffb8a10
Joy <jungheeyoo.joy@gmail.com>
2024-07-31 18:59:23 +0900

    Lists and Animations - animate bounce, spin

--- app/page.tsx
+++ app/page.tsx
@@ -144,20 +144,18 @@
 // Lists and Animations
 // odd: , even: 홀짝 스타일 주기
 // first, last
+// animation bounce, spin
 
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {['Nico', 'Me', 'You', 'Yourself'].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0"
-          >
+          <div key={index} className="flex items-center gap-5 ">
             <div className="size-10 bg-blue-400 rounded-full" />
             <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index}</span>
+            <div className="size-6 animate-spin bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>⏳</span>
             </div>
           </div>
         ))}
